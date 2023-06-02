f5173cd829e9b4af04f94a009d2d4ffb9124cb28
Faruq Hossain <faruqdigital01@gmail.com>
2023-06-02 21:33:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function updateClock() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+  
+    // Add leading zeros to minutes and seconds
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+  
+    var timeString = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("time").innerText = timeString;
+  }
+  
+  // Update the clock every second
+  setInterval(updateClock, 1000);
+  
